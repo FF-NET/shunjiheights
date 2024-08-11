7b932e04eacd87d9c6e2c5aaf15b91f9881b151a
FF-NET <166034446+FF-NET@users.noreply.github.com>
2024-08-11 23:55:21 +0900

    mmo change

--- server/mmo.js
+++ server/mmo.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io").listen(server, { log: false });
+const io = require("socket.io")(server, { log: false });
+
 
 /*****************************
       STARTING THE SERVER
